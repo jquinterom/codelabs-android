b9ac87242e3e03aec8c8ab82676f3cf6a60f51db
jquinterom <jfredyquintero@hotmail.com>
2022-03-16 11:27:18 -0500

    Primer test instrumentado

--- cameraxapp/app/src/androidTest/java/com/example/cameraxapp/MainActivityTest.kt
+++ cameraxapp/app/src/androidTest/java/com/example/cameraxapp/MainActivityTest.kt
@@ -2,14 +2,16 @@ package com.example.cameraxapp
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-
+@LargeTest
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
@@ -17,11 +19,12 @@ class MainActivityTest {
     var activityRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
-    fun clickBottom() {
+    fun buttonClick_returnSuccess() {
+        // Hacemos clic en el boton ibSum
+        onView(withId(R.id.video_capture_button))
+            .perform(click())
 
-
-        activityRule.scenario.onActivity { activity ->
-            onView(withId(R.id.image_capture_button)).perform(click())
-        }
+        onView(withId(R.id.video_capture_button))
+            .check(matches(withText("STOP CAPTURE")))
     }
 }
